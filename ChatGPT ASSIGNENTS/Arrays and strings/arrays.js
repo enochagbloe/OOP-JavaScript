9717efc12501148b1badcd3444211e3ca88da304
Enoch <enochagbloe16@gmail.com>
2025-08-19 00:57:20 +0100

    ARRAYS PART ONE

---
+++ ChatGPT ASSIGNENTS/Arrays and strings/arrays.js
@@ -0,0 +1,23 @@
+// an array is list of items
+// common operations like
+// push = Add and, remove = pop
+
+// for iterating we use a for loop or a for of loop
+
+//Assignments
+let tempurature = [1, "4", {
+    id: 1,
+    name: "John",
+}, {
+    "id": "numbers",
+    "name": "John",
+}]
+
+// lets use the common operations for this varaible
+let any = {
+    name: "John",
+    age: 23
+}
+tempurature.push(any)
+
+console.log(tempurature)
