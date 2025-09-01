7b8197f304ece576d59fcb341bd8d1c1aeea6686
Enoch <enochagbloe16@gmail.com>
2025-09-01 01:45:41 +0100

    reverse an array

--- ChatGPT ASSIGNENTS/Arrays and strings/reverse an array.js
+++ ChatGPT ASSIGNENTS/Arrays and strings/reverse an array.js
@@ -15,3 +15,14 @@ function setArrays(array) {
 }
 setArrays([1,2,3,4,5,6,7,8,9,10])
 console.log(setArrays)
+
+
+//reversing an array
+// create a function
+// create 2 variables
+// yse a while loops and agains the variables created
+// create a vraible in the loop and set it to the array[vaiable 1]
+// then array[variable one] = array[vaiable 2]
+// array[v2] to the temp
+// then increment the the variables
+// close the loop and return the array.
